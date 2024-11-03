2ff4d14bd67c6be60ef18796dcf2adb998880e85
Justin Mello <justinmello@u.boisestate.edu>
2024-11-02 23:55:50 -0600

    lab finished. CSS updates needed.

--- README.md
+++ README.md
@@ -2,23 +2,38 @@
 
 ## Overview
 
-TODO: Write a project description
+Justin Mello
+CS 408
+Fall 2024
+Module 11: Lab 11.2
+
+# Project Description
+In this project we built a very simple inventory app. This app demonstrated the use of a serverless website,using AWS to implement our backend. This was simply a continuation of the prior lab with a visual component. XML requests were used instead of fetch to put, get, and delete data. Data was returned and displayed dynamically on a web page, and also allowed for user submitted data to be added.
+
+## Running the Website
+
+1. To run everything you will need to Fork or Clone this repository and open in up in an IDE on your local PC or in VS Codespaces.
+
+2. Once you have cloned the Repo, open it up in VS Code.
+
+3. Select the index.html file from the Explorer Window.
+
+4. Then, using the VS Code Command Palette, type: > Live Preview Start Server. If this command is not available, you may need to install the VS Code Live Preview extension.
+
+5. This will open up a website preview within your IDE. You can also open this in an external browser if you choose, by clicking on the 3 horizontal lines icon to the top right of the preview window and selecting 'Open in Browser'.
+
+6. Load table data to see what currently exists in the inventory app
+
+7. Delete table data and watch the table update.
+
+8. Add your own data to the inventory app to see the table update.
+
+9. Addionally you can open your developer tool pane within a Chrome browser, and check the console to see the confirmation messages when items are loaded, added, or deleted.
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
+- Event Delegation: https://medium.com/@yurchenkor33/binding-event-listeners-to-dynamically-created-elements-in-javascript-a2e30d156b8e
+- XML: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+- Event Listeners: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener Sound effects: https://freesound.org/people/...
+- Query Selector: https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
 
-A simple bulleted list below is sufficient. For example:
-
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
--- js/main.js
+++ js/main.js
@@ -1,18 +1,135 @@
 window.onload = loaded;
 
+//Using the same endpoint so make available globally
+const apiRoute = "https://reeiitr1qg.execute-api.us-east-2.amazonaws.com/items";
+
+//adding table globally to make it easier to update....not ideal
+const tableBody = document.querySelector("#data-table tbody");
+
 /**
  * Simple Function that will be run when the browser is finished loading.
+ * This sets our event listeners for submitted items, and table loading.
+ * It prevents them from running by default, then calls the appropriate functions
+ * for additonal handling.
  */
 function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+    document.getElementById("item-submission").addEventListener('submit', function(e){
+        e.preventDefault();
+        submitData();
+        loadTable();
+        e.target.reset();
+    });
+    document.getElementById("load-data").addEventListener('click', function(e){
+        e.preventDefault();
+        loadTable(); 
+    });
+}
+
+
+/**
+ * This function loads the data table with the provided xhr request.
+ * There is some added functionality to loop through the xhr response,
+ * getting each data value of the json object returned, and dynamically adding
+ * that value to an html table. For each object a row is created.
+ * In each row, a delete button is added referencing the unique id of the item 
+ * referenced by the json object.
+ * 
+ * This is used for the delete function.
+ */
+export function loadTable(){
+    console.log("loading table");
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", apiRoute);
+    xhr.responseType ='json'
+    xhr.addEventListener("load", function () {
+    
+        if (xhr.status === 200) {
+            tableBody.innerHTML = ''; //clears table to reload data
+        }
+
+        //loop through response, adding a row for each object, and a column for each
+        //objects data including a delete button storing the item id as it's value
+        xhr.response.forEach(item => {
+            console.log(xhr.response);
+            const row = document.createElement('tr');
+            
+            row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.name}</td>
+                <td>${item.price}</td>
+                <td><button class="delete-button" value=${item.id}>Delete</button></td>
+            `;
+            
+            //add the data to the table
+            tableBody.appendChild(row);
+        });
+        //adds event listerners for each button
+        deleteButtonSetup();
+    });
+    xhr.send();
+};
+
+/**
+ * This function simply adds event listeners to each delete button created when the table is loaded.
+ * It adds an onclick event to listen for a button press.
+ * If the button is pressed, it's value is passed to delete row, a Delete Request is made, and the row
+ * is removed.
+ */
+export function deleteButtonSetup(){
+    //grab all delete buttons
+    const deleteButtons = document.querySelectorAll(".delete-button");
+    //loop through each button, add a click event listener, and on click, call deleterow
+    deleteButtons.forEach(function(button){
+        button.addEventListener('click', function() {
+            deleteRow(button.value, button); // Call deleteRow and pass button value
+        });
+    });
+};
+
+
+/**
+ * This function simply deletes the item from the database.
+ * @param {} deleteId - the item to be deleted
+ */
+
+export function deleteRow(deleteId, button){
+                
+    let xhr = new XMLHttpRequest();
+    xhr.open("DELETE", `${apiRoute}/${deleteId}`);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            let row = button.closest('tr');
+            row.innerHTML = '';  
+            console.log("successfully deleted: " + deleteId);   
+        }
+    };
+    
+    xhr.send();
 }
 
 /**
- * This function returns the string 'hello'
- * @return {string} the string hello
+ * This function submits user provided data.
+ * It creates a payload based on the user input to the page form.
+ * That payload is sent in an XML request to put the data into our database
+ * It then returns, and after load table is called to show the new data.
  */
-export function sayHello() {
-    return 'hello';
+export function submitData(){
+   
+    //get data from form
+    let payload ={
+        id: document.getElementById('item-id').value,
+        name: document.getElementById('item-name').value,
+        price: document.getElementById('item-price').value
+    }
+
+    console.log(payload)
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", apiRoute);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(payload));
+    console.log("Successfully added: " + payload.id + "," + payload.name + "," + payload.price)
 }
+
